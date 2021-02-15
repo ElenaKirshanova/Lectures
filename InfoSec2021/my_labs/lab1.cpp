500398a63147b3b578cd0e4b675e38317570dddf
elena <elenakirshanova@gmail.com>
2021-02-15 09:49:13 +0200

    lattices lec 1 and 2 scribes

--- InfoSec2021/my_labs/lab1.cpp
+++ InfoSec2021/my_labs/lab1.cpp
@@ -1,11 +1,13 @@
 //
 //  lab1.cpp
-//  
+//
 //
 //  Created by Elena on 18/01/2021.
 //
 
-#include "lab1.hpp"
+// TODO: command line
+
+//#include "lab1.hpp"
 
 #include "cryptlib.h"
 #include "secblock.h"
@@ -32,7 +34,7 @@ void Enc(SecByteBlock &key, SecByteBlock &iv, std::string &plaintext, std::strin
 	// Encryption object
 	ChaCha::Encryption enc;
 	enc.SetKeyWithIV(key, key.size(), iv, iv.size());
-	
+
 	// Perform the encryption
 	ciphertext.resize(plaintext.size());
 	enc.ProcessData((byte*)&ciphertext[0], (const byte*)plaintext.data(), plaintext.size());
@@ -43,7 +45,7 @@ void Dec(SecByteBlock &key, SecByteBlock &iv, std::string &ciphertext, std::stri
 	// Decryption object
 	ChaCha::Decryption dec;
 	dec.SetKeyWithIV(key, key.size(), iv, iv.size());
-	
+
 	// Perform the decryption
 	decrypted.resize(ciphertext.size());
 	dec.ProcessData((byte*)&decrypted[0], (const byte*)ciphertext.data(), ciphertext.size());
@@ -52,19 +54,19 @@ void Dec(SecByteBlock &key, SecByteBlock &iv, std::string &ciphertext, std::stri
 int main()
 {
 	std::string plaintext = "some plaintext", ciphertext, decrypted;
-	
+
 	SecByteBlock key(32), iv(8);
 	KeyGen(key, iv);
-	
+
 	std::cout << "Plaintext: " << plaintext << std::endl;
-	
+
 	Enc(key, iv, plaintext, ciphertext);
-	
+
 	std::cout << "Ciphertext: " << ciphertext << std::endl;
-	
+
 	Dec(key, iv, ciphertext, decrypted);
-	
+
 	std::cout << "Decrypted: " << decrypted << std::endl;
-	
+
 	return 0;
 }
